7eef8d23124a1f3b4cd88aa9cad72e61cf4f2d38
aemetz <aemetz38@gmail.com>
2023-02-06 18:01:10 -0600

    Door fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,10 +13,13 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
 
+    private bool doorOpen;
+
     // Start is called before the first frame update
     void Start()
     {
         //rb = GetComponent<Rigidbody2D>();
+        doorOpen = false;
     }
 
     // Update is called once per frame
@@ -32,10 +35,15 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.collider.gameObject.CompareTag("Door"))
         {
-            //Destroy(collision.collider.gameObject);
-            GameObject door = collision.collider.gameObject;
-            door.transform.localScale = new Vector3(0.5f, 2, 1);
-            door.transform.localPosition = new Vector3(door.transform.position.x-1, door.transform.position.y+1, door.transform.position.z); 
+            if (!doorOpen)
+            {
+                doorOpen = true;
+                //Destroy(collision.collider.gameObject);
+                GameObject door = collision.collider.gameObject;
+                door.transform.localScale = new Vector3(0.5f, 2, 1);
+                door.transform.localPosition = new Vector3(door.transform.position.x - 1, door.transform.position.y + 1, door.transform.position.z);
+            }
+            
         }
     }
 
