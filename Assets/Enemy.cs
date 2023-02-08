a64f31b6709e09f7feaa3906dff1e3412c9c2577
Fernando-00 <fernandorosell2024@u.northwestern.edu>
2023-02-08 17:16:27 -0600

    Added enemy spawn system
    -enemies spawn in waves once player leaves the door

---
+++ Assets/Enemy.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+
+
+    /// <summary>
+    /// Our rigid body component
+    /// Used to apply forces so we can move around
+    /// </summary>
+    private Rigidbody2D rigidBody;
+
+    /// <summary>
+    /// Vector from us to the player
+    /// </summary>
+    private Vector2 OffsetToPlayer;
+
+    /// <summary>
+    /// Unit vector in the direction of the player, relative to us
+    /// </summary>
+    private Vector2 HeadingToPlayer;
+
+    public Transform playerChase;
+    public float enemySpeed;
+    public float enemyDistance;
+    public float enemyStopDistance;
+
+    public float currentTime;
+    public float shootingSpeed;
+    public float maxHealth;
+    public float health;
+
+
+    void Start()
+    {
+        playerChase = GameObject.FindGameObjectWithTag("Player").transform;
+        enemyStopDistance = 0f;
+        enemyDistance = 2f;
+
+
+        currentTime = Time.time;
+
+        //HealthBar.SetHealth(health, maxHealth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OffsetToPlayer = playerChase.position - transform.position;
+        HeadingToPlayer = OffsetToPlayer.normalized;
+
+
+        if (Vector2.Distance(transform.position, playerChase.position) > enemyStopDistance)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, playerChase.position, enemySpeed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, playerChase.position) < enemyStopDistance && Vector2.Distance(transform.position, playerChase.position) > enemyDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, playerChase.position) < enemyDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerChase.position, -enemySpeed * Time.deltaTime);
+        }
+
+
+    }
+
+    public void Spawn()
+    {
+        gameObject.SetActive(true);
+    }
+   
+
+
+
+    void Destruct()
+    {
+        Destroy(gameObject);
+    }
+
+}
