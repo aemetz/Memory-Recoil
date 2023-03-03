5bb5d9afd331c22e20cf666fbbcc808fa1c75be4
aemetz <aemetz38@gmail.com>
2023-03-02 19:10:40 -0600

    Functional main menu, other stuff

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log("Test quit");
+        Application.Quit();
+    }
+
+}
