3ce5eaca35b2a1bc81219d03ff9844a9a9bb7ac3
aemetz <aemetz38@gmail.com>
2023-03-08 19:52:48 -0600

    final changes before build

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
 
     public void Menu()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 
     public void GameOver()
